c542d5232b6df04eaf2fd320a999a58d69bd0d0b
Timthom <monsson47thomas@gmail.com>
2025-11-20 11:57:04 +0100

    add final js

--- app.js
+++ app.js
@@ -1,3 +1,96 @@
 //Drum kit
 
-alert("Hello!");
+let numberOfDrumButton = document.querySelectorAll(".drum").length;
+
+for (i = 0; i < numberOfDrumButton; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function checkClick() {
+
+       /*  alert("works");
+        console.log(this);
+        this.style.color = "White"; */
+
+        let buttonInnerHTML = this.innerHTML;
+
+        makeSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML);
+
+    })
+
+
+}
+
+
+//Funktion för ljud
+
+function makeSound(key) {
+    switch (key) {
+
+        case "w":
+            let tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            let tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            let tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            let tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            let snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            let hihat = new Audio("sounds/open-hihat.mp3");
+            hihat.play();
+            break;
+        case "l":
+            let kickBass = new Audio("sounds/kick-bass.mp3");
+            kickBass.play();
+            break;
+        case "m":
+            let crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "n":
+            let clap = new Audio("sounds/clap.mp3");
+            clap.play();
+            break;
+        default:
+            console.log(buttonInnerHTML);
+            break;
+
+    }
+} //END makeSound
+
+//Detekting keyboard press
+document.addEventListener("keydown", function (event) {
+
+    console.log(event.key);
+
+    makeSound(event.key);
+    buttonAnimation(event.key);
+
+});
+
+
+//Animation funktion
+function buttonAnimation(currentkey) {
+
+    let activeButton = document.querySelector("." + currentkey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function () {
+        activeButton.classList.remove("pressed");
+
+    }, 100);
+
+}
